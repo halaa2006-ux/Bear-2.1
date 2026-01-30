9987f8339a3af23b731d50678b2922ab5cd22fff
halaa2006-ux <halaa2006@hotmail.com>
2026-01-30 13:29:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const leftEar = document.getElementById("leftEar");
+const rightEar = document.getElementById("rightEar");
+const bear = document.getElementById("bear");
+const face = document.getElementById("face");
+const rightEye = document.getElementById("rightEye");
+const rightPupil = rightEye.querySelector(".pupil");
+const winkSound = document.getElementById("winkSound");
+
+ 
+bear.addEventListener("mouseenter", () => {
+  // tilt ears outward
+  leftEar.style.transform = "rotate(-15deg)";
+  rightEar.style.transform = "rotate(15deg)";
+});
+
+bear.addEventListener("mouseleave", () => {
+  // reset ears
+  leftEar.style.transform = "rotate(0deg)";
+  rightEar.style.transform = "rotate(0deg)";
+});
+
+face.addEventListener("mouseenter", () => {
+  rightEye.style.height = "0px";
+  rightEye.style.borderRadius = "10px";
+  rightPupil.style.opacity = "0";
+  rightEye.classList.add("no-liner");
+  winkSound.currentTime = 0;
+winkSound.play();
+
+});
+
+face.addEventListener("mouseleave", () => {
+rightEye.style.height = "60px";
+  rightEye.style.borderRadius = "50%";
+  rightPupil.style.opacity = "1";
+  rightEye.classList.remove("no-liner");
+});
+
+document.body.addEventListener("click", () => {
+  winkSound.play();
+});
